0fecf0ed52d0fc6e5daeed82f9c87e9714a376f4
Matheus Geiger <matheus.slgd@gmail.com>
2022-02-16 17:40:07 -0300

    feat: remove no necessary files

--- django_template/apps/example/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from django_template.apps.example.models import AuditAction
-from django_template.support.django_helpers import CustomModelAdminMixin
-
-
-@admin.register(AuditAction)
-class AuditActionAdmin(CustomModelAdminMixin, admin.ModelAdmin):
-    search_fields = ["user_id"]
--- tests/api_conftest.py
+++
@@ -1,8 +0,0 @@
-import pytest
-
-from rest_framework.test import APIClient
-
-
-@pytest.fixture
-def api_client() -> APIClient:
-    return APIClient()
